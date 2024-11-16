027e784a7e0b621c6fdea4d561a354b5825d53da
yousrabarhdadi <yousra.barhdadi@vub.be>
2024-11-16 17:24:11 +0100

    Leaderbord medals. Created pixel images and display them on leaderboard.

--- client/src/components/leaderboard/Leaderboard.tsx
+++ client/src/components/leaderboard/Leaderboard.tsx
@@ -10,6 +10,19 @@ interface LeaderboardProps{
     users: LeaderboardEntry[]; // the list of users for the leaderboard
 }
 
+const getMedal = (index: number) => {
+    switch (index) {
+        case 0:
+            return <img src={"src/assets/goldmedal.png"} alt="Gold Medal" className="medal" />;
+        case 1:
+            return <img src={"src/assets/silvermedal.png"} alt="Silver Medal" className="medal" />;
+        case 2:
+            return <img src={"src/assets/bronzemedal.png"} alt="Bronze Medal" className="medal" />;
+        default:
+            return <span className="rank">{index+1}</span>;
+    }
+};
+
 
 const Leaderboard: React.FC<LeaderboardProps> = ({ users }) => {
     // sort the users by points in descending order
@@ -21,6 +34,7 @@ const Leaderboard: React.FC<LeaderboardProps> = ({ users }) => {
             <ol>
                 {sortedUsers.map((user, index) => (
                     <li key={index}>
+                        {getMedal(index)}
                         {user.name} - {user.points} points
                     </li>
                 ))}
