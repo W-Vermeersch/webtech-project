7d204f202ef64e5c373d6c120929c61a24e91289
yousrabarhdadi <yousra.barhdadi@vub.be>
2024-11-26 18:14:21 +0100

    Homepage feed.

--- client/src/pages/Homepage.tsx
+++ client/src/pages/Homepage.tsx
@@ -1,4 +1,5 @@
 import Leaderboard from "../components/leaderboard/Leaderboard.tsx";
+import PostFeed from "../components/postfeed/Feed.tsx";
 import "./Homepage.css"
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -16,10 +17,36 @@ const Homepage = () => {
         { name: "Ozioma", points: 1000}
     ];
 
+    const mockPosts = [
+        {
+            id:1,
+            username: "Alice",
+            content: "Just captured a majestic lion!",
+            comments: ["Amazing!", "Wow, congrats!"],
+        },
+
+        {
+            id: 2,
+            username: "Bob",
+            content: "Exploring new areas... Found a rare bird!",
+            comments: ["That's awesome!", "Where was this?"],
+        },
+
+        {
+            id: 3,
+            username: "Diana",
+            content: "Loving this game! Just hit 100 points!",
+            comments: ["Great work!", "Keep it up!"],
+        },
+    ];
+
     return (
         <Container>
             <Row className={'justify-content-end'}>
                 {/* for future components like map and postfeed */}
+                <Col xs={9}>
+                    <PostFeed posts={mockPosts} />
+                </Col>
                 <Col xs={3}>
                     <Leaderboard users={mockUsers} />
                 </Col>
