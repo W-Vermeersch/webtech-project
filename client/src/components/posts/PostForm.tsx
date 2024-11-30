4a625e5cbe8fcf76995a1997cadffae52a68871e
yousrabarhdadi <yousra.barhdadi@vub.be>
2024-11-30 19:14:04 +0100

    Basic form copied from bootstrap react website.

--- client/src/components/posts/PostForm.tsx
+++ client/src/components/posts/PostForm.tsx
@@ -1,3 +1,5 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 const PostForm = () => {
     // define your form
@@ -6,7 +8,28 @@ const PostForm = () => {
 
 
     return (
-        <>PostForm</>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className="text-muted">
+                 We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+             </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+             </Form.Group>
+
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+    </Form>
     )
 }
 
