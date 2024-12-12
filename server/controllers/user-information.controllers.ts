2fd6248ca70ff3c4ab724cdd56c044fb8f51e26e
Ozioma <ozioma.olisa@vub.be>
2024-12-12 10:26:56 +0100

    fix profile info fetch + pageNotFound to home at "/"

--- server/controllers/user-information.controllers.ts
+++ server/controllers/user-information.controllers.ts
@@ -4,61 +4,75 @@ import { UserAuthentificationController } from "./user-authentification/base.use
 import Database from "../database";
 
 export class UserInfoController extends UserAuthentificationController {
+  constructor(private db: Database) {
+    super();
+  }
 
-    constructor(private db: Database) {
-        super();
+  initializeRoutes(): void {
+    // /user/get
+    this.router.get(
+      "/get-profile-information",
+      authenticateToken,
+      (req: express.Request, response: express.Response) => {
+        console.log("authenticated");
+        return this.getUserInformation(req, response);
+      }
+    );
+
+    this.router.get(
+      "/get-user-comments",
+      authenticateToken,
+      (req: express.Request, response: express.Response) => {
+        console.log("authenticated");
+        return this.getUserComments(req, response);
+      }
+    );
+  }
+
+  private async getUserInformation(
+    req: express.Request,
+    res: express.Response
+  ) {
+    const username = req.query.username ? req.query.username : " ";
+
+    const users = await this.db.fetchUserUsingUsername(username.toString());
+    if (users.length === 0) {
+      res.json({
+        redirect: "/home",
+      });
+    } else {
+      const userObject = users[0];
+
+     
+      const userProfileDecoration = (await this.db.fetchProfileDecoration(userObject.user_id))[0];
+
+      res.json({
+        username: userObject.username,
+        user_id: userObject.user_id,
+        displayname: userProfileDecoration.display_name,
+        profilepicture: userProfileDecoration.profile_picture_image_url,
+        bio: userProfileDecoration.bio,
+        totalexp: userProfileDecoration.total_exp,
+        badges: userProfileDecoration.badges,
+      });
     }
+  }
 
-    initializeRoutes(): void {
-        // /user/get 
-        this.router.get("/get-profile-information", authenticateToken, (req: express.Request, response: express.Response) => {
-           console.log("authenticated")
-           return this.getUserInformation(req, response);
-        });
-
-         this.router.get("/get-user-comments", authenticateToken, (req: express.Request, response: express.Response) => {
-            console.log("authenticated")
-            return this.getUserComments(req, response);
-        });
+  private async getUserComments(req: express.Request, res: express.Response) {
+    const username = req.query.username ? req.query.username : " ";
+    const users = await this.db.fetchUserUsingUsername(username.toString());
+    if (users.length === 0) {
+      res.json({
+        redirect: "/pageNotFound",
+      });
+    } else {
+      const userObject = users[0];
+      const userComments = await this.db.fetchCommentsOfUser(
+        userObject.user_id
+      );
+      res.json({
+        user_posts: userComments,
+      });
     }
-
-    private async getUserInformation(req: express.Request, res: express.Response) {
-        const username = (req.query.username ) ? req.query.username : " ";
-
-        const users = await this.db.fetchUserUsingUsername(username.toString())
-        if (users.length === 0) {
-            res.json({
-                redirect: '/home'
-            });
-        } else {
-            const userObject = users[0]
-            const userProfileDecoration = await this.db.fetchProfileDecoration(userObject.user_id);
-            //console.log(userProfileDecoration); // empty array??
-            res.json({
-                username: userObject.username,
-                user_id: userObject.user_id,
-                displayname: userProfileDecoration.display_name,
-                profilepicture: userProfileDecoration.profile_picture_image_url,
-                bio: userProfileDecoration.bio,
-                totalexp: userProfileDecoration.total_exp,
-                badges: userProfileDecoration.badges
-            });
-        }
-    }
-
-    private async getUserComments(req: express.Request, res: express.Response) {
-        const username = (req.query.username ) ? req.query.username : " ";
-        const users = await this.db.fetchUserUsingUsername(username.toString())
-        if (users.length === 0) {
-            res.json({
-                redirect: '/pageNotFound'
-            });
-        } else {
-            const userObject = users[0]
-            const userComments = (await this.db.fetchCommentsOfUser(userObject.user_id))
-            res.json({
-                user_posts: userComments
-            });
-        }
-    }
+  }
 }
