2b6acc055234feeed7a44692a0cac8a602af2d8b
yousrabarhdadi <yousra.barhdadi@vub.be>
2024-12-14 12:52:16 +0100

    fixed liking

--- client/src/components/scrollerPagination/SinglePost.tsx
+++ client/src/components/scrollerPagination/SinglePost.tsx
@@ -25,19 +25,20 @@ const SinglePost = ({ post }: SinglePostProps) => {
   const [showCommentModal, setShowCommentModal] = useState(false);
 
   const handleLiking = async () => {
+    console.log("Handlelike has been called");
     const post_id = post.idx;
     const resp = await axiosPrivate.get(LIKE_POST, { params: { post_id } });
     if (resp.status === 200) {
       setLikes((prev) => prev + 1);
       setIsLiked(!isLiked);
     }
-    console.log("Handlelike has been called");
+    console.log("amount of likes", isLiked);
   };
 
   const handleUnliking = async () => {
     const post_id = post.idx;
     console.log("handleunliking has been called");
-    const resp = await axiosPrivate.post(DELETE_LIKE, {
+    const resp = await axiosPrivate.get(DELETE_LIKE, {
       params: { post_id },
     });
     if (!(resp.status === 404)) {
