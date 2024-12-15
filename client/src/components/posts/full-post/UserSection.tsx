59dfa9bfd9cd84a590484644caa1cbb66adba415
Ozioma <ozioma.olisa@vub.be>
2024-12-15 22:54:08 +0100

    add link on profile elements full post

--- client/src/components/posts/full-post/UserSection.tsx
+++ client/src/components/posts/full-post/UserSection.tsx
@@ -2,6 +2,7 @@ import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
+import { NavLink } from "react-router-dom";
 
 interface UserSectionProps {
   username: string;
@@ -17,18 +18,30 @@ export default function UserSection({
   return (
     <Row>
       <Col xs="auto">
-        <Image src={profile_pic} alt="profile pic" roundedCircle width={150} height={150}/>
+        <NavLink to={`/profile/${username}`}>
+          <Image
+            src={profile_pic}
+            alt="profile pic"
+            roundedCircle
+            width={150}
+            height={150}
+          />
+        </NavLink>
       </Col>
       <Col className="p-1">
         <Row>
-          <h2 className="text-light mt-2">{username}</h2>
+          <NavLink to={`/profile/${username}`}>
+            <h2 className="text-light mt-2">{username}</h2>
+          </NavLink>
         </Row>
         <Row>
           <Col>
             <h3 className="text-light">{"Level " + level}</h3>
           </Col>
           <Col className="d-flex justify-content-end">
-            <Button className="me-3" variant="outline-light">Follow</Button>
+            <Button className="me-3" variant="outline-light">
+              Follow
+            </Button>
           </Col>
         </Row>
       </Col>
