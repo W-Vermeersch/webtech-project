8fdb5d0546ce111e2e8dc2d48a94b107bed97e3a
yousrabarhdadi <yousra.barhdadi@vub.be>
2024-12-16 13:05:08 +0100

    Working on leaderboard

--- client/src/components/leaderboard/Leaderboard.tsx
+++ client/src/components/leaderboard/Leaderboard.tsx
@@ -1,9 +1,12 @@
 import "./Leaderboard.css";
+import axios from "../../api/axios.ts";
+import { FETCH_LEADERBOARD } from "../../api/urls";
+import { useEffect, useState } from "react";
 
 // define the type of leaderboard entries, depends on how backend passes it, for now defined as username and points
 interface LeaderboardEntry {
-  name: string;
-  points: number;
+  username: string;
+  totalexp: number;
 }
 
 interface LeaderboardProps {
@@ -11,19 +14,33 @@ interface LeaderboardProps {
 }
 
 export default function Leaderboard({ users }: LeaderboardProps) {
-  // sort the users by points in descending order
-  const sortedUsers = [...users].sort((a, b) => b.points - a.points);
+  const [leaderboard, setLeaderboard] = useState<LeaderboardEntry[]>([]); // State to hold the leaderboard data
+  const [isLoading, setIsLoading] = useState<boolean>(true); // State to track loading status
+
+  //fetch the leaderboard
+  useEffect(() => {
+    async function fetchLeaderboard() {
+      const resp = await axios.get(FETCH_LEADERBOARD);
+      console.log("This is the leaderboard", resp.data);
+    }
+  });
 
   return (
-    <>
-      <ol>
-        {sortedUsers.map((user, index) => (
-          <li key={index}>
-            <span className="rank">{index + 1}</span>
-            {user.name} - {user.points} points
-          </li>
-        ))}
-      </ol>
-    </>
+    <div className="leaderboard-container">
+      <h1 className="leaderboard-title">LEADERBOARD</h1>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ol className="leaderboard-list">
+          {leaderboard.map((user, index) => (
+            <li key={index} className="leaderboard-entry">
+              <span className="rank">{index + 1}.</span>
+              <span className="username">{user.username}</span>
+              <span className="points">{user.totalexp} XP</span>
+            </li>
+          ))}
+        </ol>
+      )}
+    </div>
   );
 }
