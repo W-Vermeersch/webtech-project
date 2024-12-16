cc382fa2d618b565156f1227fb10a96b5d8d6b23
Ozioma <ozioma.olisa@vub.be>
2024-12-16 16:27:03 +0100

    continue search (no fetch)

--- client/src/geocoding.ts
+++ client/src/geocoding.ts
@@ -12,9 +12,15 @@ import {
 
 export const mapsAPIKey = "AIzaSyAxcoTGl9eGJr427kNv1YwOObWbkXPE1no";
 
-export async function getCityCountry(latitude: number, longitude: number): Promise<{ state: string; country: string }> {
+export async function getCityCountry(
+  latitude: number,
+  longitude: number
+): Promise<{ state: string; country: string }> {
   try {
-    const { results } = await geocode(RequestType.LATLNG, `${latitude},${longitude}`);
+    const { results } = await geocode(
+      RequestType.LATLNG,
+      `${latitude},${longitude}`
+    );
 
     interface AddressComponent {
       long_name: string;
@@ -23,7 +29,10 @@ export async function getCityCountry(latitude: number, longitude: number): Promi
     }
 
     const { state, country } = results[0].address_components.reduce(
-      (acc: { state: string; country: string }, component: AddressComponent) => {
+      (
+        acc: { state: string; country: string },
+        component: AddressComponent
+      ) => {
         if (component.types.includes("locality")) {
           acc.state = component.long_name;
         } else if (component.types.includes("country")) {
@@ -35,10 +44,21 @@ export async function getCityCountry(latitude: number, longitude: number): Promi
     );
 
     return { state, country };
-
   } catch (error) {
     console.error(error);
     return { state: "", country: "" };
+  }
+}
+
+export async function getLatLng(address: string) {
+  try {
+    await geocode(RequestType.ADDRESS, address).then(({ results }) => {
+      const { lat, lng } = results[0].geometry.location;
+      return [lat, lng];
+    });
+  } catch (error) {
+    console.error(error);
+    return [0, 0];
   }
 }
 
